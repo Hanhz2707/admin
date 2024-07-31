23fcc39749d6b079ff3dea2e2e011da26bb43da9
Pham Huy Anh <phamhuyanh277@gmail.com>
2024-07-31 13:54:51 +0300

    Setting up Orders Page

--- src/pages/Orders.jsx
+++ src/pages/Orders.jsx
@@ -1,9 +1,15 @@
-import React from 'react'
+import React from 'react';
+import {GridComponent, ColumnsDirective, ColumnDirective, Page, Inject, Resize, Sort, ContextMenu, Filter, ExcelExport, PdfExport, Edit} from '@syncfusion/ej2-react-grids';
+import {ordersData, contextMenuItems, ordersGrid} from '../data/dummy';
+import {Header} from '../components';
+
 
 const Orders = () => {
   return (
-    <div>Orders</div>
-  )
-}
+    <div>
+      <Header category='Page' title='Orders' />
+    </div>
+  );
+};
 
-export default Orders
+export default Orders;
